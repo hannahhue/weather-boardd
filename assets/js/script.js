a93162ffa7dc145ad117ce4dcc16743a29446641
unknown <alicia9790972@gmail.com>
2022-09-24 02:19:28 -0400

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//assign
+var historyContainer = $("#historyContainer");
+var searchBar = $("#bar");
+var searchBtn = $("#searchBtn");
+var infoContainer = $("#infoContainer");
+var fiveDay = $("#5day");
+
+//search input
