0beda40565d5de17dba4369688dea6635d0f5a99
unknown <alicia9790972@gmail.com>
2022-09-27 08:09:24 -0400

    added new function to put long and lat together in string, trouble shoot key issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,14 @@ var searchBar = $("#bar");
 var searchBtn = $("#searchBtn");
 var infoContainer = $("#infoContainer");
 var fiveDay = $("#5day");
-var apiForecast = "https://api.openweathermap.org/data/2.5/forecast?";
+//"unauthorized key wont display ect error....401"
+var apiForecast = "https://api.openweathermap.org/data/3.0/onecall?";
 var latLong = "https://api.openweathermap.org/geo/1.0/direct?q=";
 var apiKey = "&appid=039d31a585aa282d08ff4a2d89c81fe9";
 //search input
 
+//starting function when clicked search button
+//takes info from searchbar and input into string
 function searchLatLong() {
   cityName = searchBar.val().trim();
   fetch(latLong + cityName + apiKey)
@@ -17,11 +20,27 @@ function searchLatLong() {
     })
     .then(function (data) {
       console.log(data);
-      var latt = 'lat' + data[0].lat
-      var longg = '&lon' + data[0].lon
+      var latt = "lat" + data[0].lat;
+      var longg = "&lon" + data[0].lon;
+      searchWeather(cityName, latt, longg);
     });
 }
 
-function search
+//called to start from prior function
+//inputs data into string and generates info
+function searchWeather(cityName, latt, longg) {
+  fetch(apiForecast + latt + longg + apiKey)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      displayWeather(data);
+    });
+}
+
+//api key wont work to intput info
+//should input info into container
+function displayWeather(cityInfo) {}
 
 searchBtn.on("click", searchLatLong);
